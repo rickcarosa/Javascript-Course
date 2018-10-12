1836ae50c3c1a2b8a729bccf89ce8696ee18bec5
ricardocarosa <rickcarosa@yahoo.com>
2018-10-12 16:12:00 -0400

    scope chain

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-var a = 'hello world';
-console.log(a)
+ // var a = 'hello world';
+// console.log(a)
 
 // 'undefined' means variable hasn't been set
 
-if(a === undefined) {
-    console.log('a is undefined')
-}
-else{
-    console.log('a is defined')
-}
+// if(a === undefined) {
+//     console.log('a is undefined')
+// }
+// else{
+//     console.log('a is defined')
+// }
 
 // if value of a is set it is true
 // if var a is never typed, a is not in memory and javascript engine says err, a is not defined
 // if var a has no value, it will be 'undefined'
+
+
+// javascript is single threaded 
+
+
+
+
+// SECTION 13
+
+// function b(){
+//     var myVar;
+//     console.log(myVar);
+// }
+
+// function a(){
+//     var myVar = 2;
+//     console.log.og(myVar);
+//     b();
+// }
+
+// var myVar = 1;
+// console.log(myVar)
+// a();
+
+// console will read 1, 2, undefined
+// each myVar is in it's own exectution context so it will show each myVar separately
+
+
